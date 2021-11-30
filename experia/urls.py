e2b19b0df427484f704f9ceb391ce528638d6eb2
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-11-30 20:10:46 +0000

    Added checkout bag urls and template

--- experia/urls.py
+++ experia/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('', HomeView.as_view(), name='home'),
     path('experiences/', include('experiences.urls')),
+    path('bag/', include('bag.urls')),
     # path('experiences/', ExperienceView.as_view(), name='experiences'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
