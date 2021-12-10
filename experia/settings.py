b2ba588d8a34eb5de93facb8a88492d3ae0a44b9
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-12-10 12:51:12 +0000

    Added basic checkout func with stripe intents

--- experia/settings.py
+++ experia/settings.py
@@ -183,3 +183,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Add discount % or code VARS here.
 # possibly DATE var to use as discount code at checkout.
+
+# stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
