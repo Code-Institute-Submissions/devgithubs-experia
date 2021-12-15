830e06020a13160f7484ae59141a0288e9226fd3
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-12-15 17:47:57 +0000

    Added update profile info form

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
-
+from .forms import UserProfileForm
 
 # Create your views here.
 def profile(request):
@@ -10,8 +10,12 @@ def profile(request):
     '''
     profile = get_object_or_404(UserProfile, user=request.user)
 
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
+        'form': form,
         'profile': profile,
     }
 
