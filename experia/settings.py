68e12c6e52a0e12374a91a54089a947618b9208c
devgithubs <arthur.oleary24@mail.dcu.ie>
2022-01-06 13:17:09 +0000

    fixed typo in DATABASES_URL

--- experia/settings.py
+++ experia/settings.py
@@ -129,7 +129,7 @@ WSGI_APPLICATION = 'experia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/databases
 
-if 'DATABASE_URL' in os.environ:
+if 'DATABASES_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASES_URL'))
     }
