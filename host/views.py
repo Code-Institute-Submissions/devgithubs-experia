d141d2288895c406e19d37ef15d9ec3471840f64
devgithubs <arthur.oleary24@mail.dcu.ie>
2022-01-17 17:25:15 +0000

    added update/del funcs to host view

--- host/views.py
+++ host/views.py
@@ -9,47 +9,67 @@ from .forms import PostExperienceForm
 from profiles.models import UserProfile
 
 
+def update_experience(request, item_id):
+    experience = get_object_or_404(Experiences, pk=item_id)
+    if request.method == 'POST':
+        form = PostExperienceForm(request.POST, request.FILES, instance=experience)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated experience!')
+            return redirect(reverse('experiences_detail', args=[experience.id]))
+        else:
+            messages.error(request, 'Failed to update experience. Please ensure the form is valid.')
+    else:
+        form = PostExperienceForm(instance=experience)
+        messages.info(request, f'You are editing {experience.name}')
+
+    template = 'host/update_experience.html'
+    context = {
+        'form': form,
+        'experience': experience,
+    }
+
+    return render(request, template, context)
+
+
+def remove_experience(request, created_id):
+    experience = get_object_or_404(Experiences, pk=created_id)
+    experience.delete()
+    messages.success(request, 'Removed from your experiences')
+    return redirect(reverse('experiences'))
 
 
 class PostExperienceView(TemplateView):
     '''
     Class view that handles form to create user experience
     '''
-
+    queryset = Experiences.objects.all()
     template_name = 'host/host.html'
-
-    def remove_experience(self, request, item_id):
-        experience = get_object_or_404(Experiences, pk=item_id)
-        experience.delete()
-        messages.success(request, 'Removed from your experiences')
-        return redirect(reverse('host'))
 
     def get(self, request):
 
-        for key, value in request.session.items():
-            print(f'key: {key}, value: {value}')
         user_experience = get_object_or_404(UserProfile, user=request.user)
-        form = PostExperienceForm(instance=request.user)
-        print(form)
+        form = PostExperienceForm()
+
         created_experience = user_experience.creator.all()
         context = {'experience_form': form, 'created_experience': created_experience}
 
         return render(request, self.template_name, context)
 
     def post(self, request):
-        initial_name = request.user
-        # experience = get_object_or_404(UserProfile, user=request.user)
-        form = PostExperienceForm(request.POST, request.FILES, instance=request.user)
+
+        form = PostExperienceForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            post.user = initial_name
+            user = get_object_or_404(UserProfile, user=request.user)
+            post.created_by = user
             post.save()
 
             messages.success(request, 'Successfully added Experience!')
 
             return redirect(reverse('host'))
         else:
-            form = PostExperienceForm(initial={'created_by': initial_name})
+            form = PostExperienceForm()
             messages.error(request, 'Failed to add experience. Please ensure the form is valid.')
 
         context = {'experience_form': form}
