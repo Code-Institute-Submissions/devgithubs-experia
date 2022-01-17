665f43bef8243c21a0040ea960bea6d45b4438b0
devgithubs <arthur.oleary24@mail.dcu.ie>
2022-01-17 17:23:51 +0000

    added urls to host app

--- host/urls.py
+++ host/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from . import views
 from host.views import PostExperienceView
 
 
 urlpatterns = [
     path('host', PostExperienceView.as_view(), name='host'),
-    path('remove_experience/<int:item_id>', PostExperienceView.remove_experience, name='remove_experience'),
+    path('delete/<int:created_id>', views.remove_experience, name='remove_experience'),
+    path('edit/<int:item_id>', views.update_experience, name='update_experience'),
 ]
