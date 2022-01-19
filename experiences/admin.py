6deb2329d2e6cc706baadf4ca4a17137e652ad71
devgithubs <arthur.oleary24@mail.dcu.ie>
2022-01-19 16:36:32 +0000

    register review model and format

--- experiences/admin.py
+++ experiences/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Experiences, ExperienceCategory
+from .models import Experiences, ExperienceCategory, ExperienceReview
 
 
 class ExperienceAdmin(admin.ModelAdmin):
+    '''Register of Experiences model'''
     list_display = (
         'experience_category',
         'name',
@@ -15,12 +16,28 @@ class ExperienceAdmin(admin.ModelAdmin):
     ordering = ('experience_category',)
 
 
+admin.site.register(Experiences, ExperienceAdmin)
+
+
 class CategoryAdmin(admin.ModelAdmin):
+    '''Register of Category model'''
     list_display = (
         'friendly_name',
         'name',
     )
 
 
-admin.site.register(Experiences, ExperienceAdmin)
 admin.site.register(ExperienceCategory, CategoryAdmin)
+
+
+class ExperienceReviewAdmin(admin.ModelAdmin):
+    '''Register of Review model'''
+    list_display = (
+        'user',
+        'experience',
+        'review_text',
+        'review_rating',
+    )
+
+
+admin.site.register(ExperienceReview, ExperienceReviewAdmin)
