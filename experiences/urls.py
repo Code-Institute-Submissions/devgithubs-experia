1d654e0b36ce6541ddd0af7f4f33bd848f65a255
devgithubs <arthur.oleary24@mail.dcu.ie>
2022-01-19 17:35:21 +0000

    Added review urls

--- experiences/urls.py
+++ experiences/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
-# from .views import ExperienceView
-
 
 urlpatterns = [
     path('', views.all_experiences, name='experiences'),
-    path('<experience_id>', views.experiences_detail, name='experiences_detail'),
+    path('', views.all_reviews, name='all_reviews'),
+    path('<experience_id>', views.experiences_detail,
+         name='experiences_detail'),
+    path('add/<int:experience_id>', views.add_review, name='add_review'),
+    path('edit/<int:review_id>', views.edit_review, name='edit_review'),
+    path('delete/<int:review_id>', views.delete_review, name='delete_review'),
 ]
