c4305fd3fa5cde4d718071316b0ed4b15f1db710
devgithubs <arthur.oleary24@mail.dcu.ie>
2022-01-21 18:43:17 +0000

    DEBUG: False

--- experia/settings.py
+++ experia/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['experia-app.herokuapp.com', 'localhost', 'https://8000-devgithubs-experia-5f6dfywraj0.ws-eu27.gitpod.io']
 CSRF_TRUSTED_ORIGINS = ['https://8000-devgithubs-experia-5f6dfywraj0.ws-eu27.gitpod.io']
