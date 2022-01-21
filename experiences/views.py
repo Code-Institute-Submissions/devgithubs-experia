82cd01e61ea1e0790786654fa2fbcdb57ec60a4e
devgithubs <arthur.oleary24@mail.dcu.ie>
2022-01-21 16:28:30 +0000

    Added login required decorator to add_review

--- experiences/views.py
+++ experiences/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 from .models import Experiences, ExperienceCategory, ExperienceReview
@@ -86,6 +87,7 @@ def all_reviews(request):
     return render(request, 'experiences/experiences_detail.html', context)
 
 
+@login_required
 def add_review(request, experience_id):
     """
     users can add a review to an experience
